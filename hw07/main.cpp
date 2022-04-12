2d44d65b12a274fac53f1e3fbe4bf2a706034803
Noah LaPoint <noah.lapoint@icloud.com>
2022-04-12 10:48:53 -0400

    Initial Commit

---
+++ hw07/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw07
+//
+//  Created by Noah LaPoint on 4/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
